3c1f5c1abad857837d5377f9c14123e394c36cfb
Rene <skou.rene@gmail.com>
2015-11-25 23:45:34 +0100

    refactored droppable method as out event can't be used he way it works now.

--- assets/js/heyloyalty.js
+++ assets/js/heyloyalty.js
@@ -3,6 +3,9 @@ jQuery(document).ready(function(){
     var dropItem = jQuery('.droppable');
     var dragItem = jQuery('.draggable');
     var hlContainer = jQuery('.hl-container');
+    var hidden = jQuery('#mapped-fields');
+    var mappedFields = [];
+    var removedFields = [];
 
 
     dropItem.droppable({
@@ -10,15 +13,17 @@ jQuery(document).ready(function(){
         drop : function (event,ui){
             if(jQuery(this).find('.draggable').length <= 0) {
                 jQuery(ui.draggable).detach().css({top: 0, left: 0}).appendTo(this);
+                var keyValue = jQuery(this).data('name')+"="+jQuery(ui.draggable).text();
+                mappedFields.push(keyValue);
+                hidden.val(mappedFields);
+                jQuery(ui.draggable).on('click',function(){
+                    var keyValue = jQuery(this).parent().data('name')+"="+jQuery(this).text();
+                    var index = mappedFields.indexOf(keyValue);
+                    removedFields = (index > -1) ? mappedFields.splice(index,1) : mappedFields;
+                    hidden.val(mappedFields);
+                    jQuery(this).detach().css({top:0,left:0}).appendTo('.hl-container');
+                });
             }
-        },
-        out: function (event,ui) {
-            jQuery('.draggable',this).detach().css({top:0,left:0}).appendTo('.hl-container');
-            console.log(jQuery('.draggable').data('name'));
-        },
-        over: function (event,ui){
-
         }
-
     });
 });
