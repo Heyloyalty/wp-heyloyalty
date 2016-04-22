620537f74c6835735a956fddcfdac2b09bb97a18
skourene <skou.rene@gmail.com>
2016-04-22 11:31:30 +0200

    update handler for field options

--- src/Services/HeyloyaltyServices.php
+++ src/Services/HeyloyaltyServices.php
@@ -1,40 +1,99 @@
 <?php
-
+/*
+ * This file is part of wp-heyloyalty.
+ *
+ * Copyright (c) 2015 Heyloyalty.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 namespace Heyloyalty\Services;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 
+/**
+ * Class HeyloyaltyServices.
+ * Uses the Heyloyalty api to create, update and delete members.
+ * @package Heyloyalty\Services
+ */
 class HeyloyaltyServices {
 
     const HOST = 'https://api.heyloyalty.com';
     const ENDPOINTTYPE = '/loyalty/v1';
-
+    
+    /**
+     * Create member.
+     * @param $params
+     * @param $list_id
+     * @return array|mixed
+     */
     public function createMember($params,$list_id)
     {
         $response = $this->sendRequest('POST','/lists/'.$list_id.'/members',$params);
         $response = $this->responseToArray($response);
         return $response;
     }
+    
+    /**
+     * Update member.
+     * @param $params
+     * @param $list_id
+     * @param $member_id
+     * @return array|mixed
+     */
     public function updateMember($params,$list_id,$member_id)
     {
         $response = $this->sendRequest('PUT','/lists/'.$list_id.'/members/'.$member_id,$params);
         $response = $this->responseToArray($response);
         return $response;
     }
+    
+    /**
+     * Delete member.
+     * @param $list_id
+     * @param $member_id
+     * @return array|mixed
+     */
     public function deleteMember($list_id,$member_id)
     {
         $response = $this->sendRequest('DELETE','/lists/'.$list_id.'/members/'.$member_id);
         $response = $this->responseToArray($response);
         return $response;
     }
+    
+    /**
+     * Get list by id.
+     * @param $list_id
+     * @return array|mixed
+     */
     public function getList($list_id)
     {
         $response = $this->sendRequest('GET','/lists/'.$list_id);
         $response = $this->responseToArray($response);
         return $response;
     }
-
+    
+    /**
+     * Get lists.
+     * Gets all lists from an account.
+     * @return array|mixed
+     */
     public function getLists()
     {
         $response = $this->sendRequest('GET','/lists/');
@@ -42,6 +101,7 @@ class HeyloyaltyServices {
         return $response;
     }
     /**
+     * Get credentials.
      * @desc get credentials from client
      * @return array
      */
@@ -55,6 +115,7 @@ class HeyloyaltyServices {
 
 
     /**
+     * Send request
      * @param $type
      * @param $url
      * @param array $query
@@ -91,6 +152,7 @@ class HeyloyaltyServices {
     }
 
     /**
+     * Get guzzle client.
      * @desc gets the guzzle version 6 client
      * @return object
      */
@@ -102,6 +164,7 @@ class HeyloyaltyServices {
     }
 
     /**
+     * Response handler.
      * @param $code
      * @return bool
      * @throws \Exception
@@ -125,6 +188,7 @@ class HeyloyaltyServices {
     }
 
     /**
+     * Response to array.
      * @param $response
      * @return array
      */
@@ -134,6 +198,7 @@ class HeyloyaltyServices {
     }
 
     /**
+     * Get member by email.
      * @param $client
      * @param $email
      * @return null
