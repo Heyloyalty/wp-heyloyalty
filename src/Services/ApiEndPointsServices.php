52809cade7468cc3b7e4e4dbc48fb00aa687491c
skourene <skou.rene@gmail.com>
2017-11-08 17:50:08 +0100

    Json decode string

--- src/Services/ApiEndPointsServices.php
+++ src/Services/ApiEndPointsServices.php
@@ -2,39 +2,47 @@
 
 namespace Heyloyalty\Services;
 
-use Heyloyalty\Services\WpUserServices;
 use WP_REST_Controller;
 
 class ApiEndPointsServices extends WP_REST_Controller
 {
     public $wpUserService;
-    public function __construct(){
-        add_action('rest_api_init', array($this, 'add_endpoints'),0);
+    public function __construct()
+    {
+        add_action('rest_api_init', array($this, 'add_endpoints'), 0);
         $this->wpUserService = new WpUserServices();
     }
 
     /** Add API Endpoint
-     *	@return void
      */
-    public function add_endpoints(){
+    public function add_endpoints()
+    {
         $namespace = 'wp-heyloyalty';
-        register_rest_route($namespace,'/member',array(
+        register_rest_route($namespace, '/member', array(
             'methods' => 'POST',
-            'callback' => array($this,'member_handler'),
+            'callback' => array($this, 'member_handler'),
             'args' => array(),
         ));
     }
 
     public function member_handler($request)
     {
-        $body = $request->get_params();
-        if (!isset($body['data'])) {
-            return 'No object';
+        try {
+            $body = $request->get_params();
+            if (!isset($body['data'])) {
+                return 'No object';
+            }
+            $member = $body['data'];
+            if (!is_array($member)) {
+                $member = json_decode($member, true);
+            }
+            if ($member['type'] == 'unsubscribe') {
+                return $this->wpUserService->unsubscribe($member);
+            }
+
+            return 'no member to unsubscribe';
+        } catch (Exception $e) {
+            return null;
         }
-        $member = $body['data'];
-        if ($member['type'] == 'unsubscribe') {
-            return $this->wpUserService->unsubscribe($member);
-        }
-        return 'no member to unsubscribe';
     }
 }
